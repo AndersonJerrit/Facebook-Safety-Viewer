4f55861ffc3a8ea77b16e5998ba1f53f8ff6ebf9
Jerrit <jerrit@localhost.(none)>
2015-04-08 11:34:24 -0400

    Some basic oo

---
+++ Facebook-Safety-Viewer/FacebookSafetyViewer.py
@@ -0,0 +1,6 @@
+from facebook import *
+
+class FacebookSafetyViewer:
+    
+    def __init__(self):
+        print("Hello World")
---
+++ Facebook-Safety-Viewer/runme.py
@@ -0,0 +1,4 @@
+from FacebookSafetyViewer import *
+
+if __name__ == "__main__":
+    fb  = FacebookSafetyViewer()
